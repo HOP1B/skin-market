281fd5bf62c1986ee42343e2efe25adfbed773dc
24HP9588 24HP9588 <24HP9588@admins-iMac.local>
2025-02-19 18:32:53 +0800

    feat(skincard detail): add skin card detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { MainLayout } from "./common/MainLayout";
+import ItemDetails from "./components/itemDetail";
 
 export default function Home() {
   return (
-    <MainLayout>
-      <div>hi</div>
-    </MainLayout>
+    <>
+      <MainLayout>
+        <div>hi</div>
+      </MainLayout>
+
+      <ItemDetails />
+    </>
   );
 }
